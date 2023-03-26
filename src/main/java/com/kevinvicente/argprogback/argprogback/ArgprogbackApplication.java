14b6f5a401e4d3269fe5a8cb9193707ece481337
k3v <25158905+k3v53@users.noreply.github.com>
2023-03-26 14:44:51 -0300

    Spring Initializer

---
+++ src/main/java/com/kevinvicente/argprogback/argprogback/ArgprogbackApplication.java
@@ -0,0 +1,13 @@
+package com.kevinvicente.argprogback.argprogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgprogbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgprogbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevinvicente/argprogback/argprogback/ArgprogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinvicente.argprogback.argprogback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgprogbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
